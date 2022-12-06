9bff612e951f551da5a965779403b8c5c5781ea9
Neha Ravindra Mundase <mundaseneha20@gmail.com>
2022-12-06 12:00:07 +0530

    Sub.java added

---
+++ Sub.java
@@ -0,0 +1,8 @@
+public class Sub {
+    public static void main(String[] args) {
+            int a = 2;
+            int b = 3;
+            int c =(a-b);
+            System.out.println(c);
+    }
+}
